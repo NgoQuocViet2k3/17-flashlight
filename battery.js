7a02798fc7d0def8a653772f028ac7123ef3be30
ruukichi <ngovietlc93@gmail.com>
2021-12-23 14:26:56 +0700

    xayy dung lop mo phong su hoat dong cua den pin

---
+++ battery.js
@@ -0,0 +1,22 @@
+class Battery {
+    energy;
+
+    constructor(energy) {
+        this.energy = energy
+    }
+
+    getEnergy() {
+        return this.energy;
+    }
+
+    setEnergy(energy) {
+        this.energy = energy
+    }
+
+    decreaseEnergy() {
+        if (this.energy > 0) {
+            this.energy--;
+        }
+    }
+
+}
---
+++ flashlamp.js
@@ -0,0 +1,32 @@
+class FlashLamp {
+    battery;
+
+    constructor(battery) {
+        this.battery = battery;
+
+    }
+
+    getBatteryInfo() {
+        return this.battery.getEnergy();
+    }
+
+    setBattery(battery) {
+        this.battery = battery;
+    }
+
+    light() {
+        if (this.status) {
+            alert("Lighting");
+        } else {
+            alert("Not lighting");
+        }
+    }
+
+    turnOn() {
+        this.status = true;
+    }
+
+    turnOff() {
+        this.status = false;
+    }
+}
